5a05f36ac23e4a718fb9bdc5fe56ef1b097260b7
Sean Marcia <smarcia@gmail.com>
2023-09-27 13:25:52 -0400

    Clean up

--- components/GitHubOctocat.tsx
+++ components/GitHubOctocat.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const GitHubOctocat = () => {
   return (
-    <div className="absolute z-10 right-80 top-60 hidden sm:block">
+    <div className="absolute z-10 right-40 top-60 hidden sm:block">
       <Image src="/octocatwithbody.svg" alt="First Issue" width={384} height={384} />
     </div>
   );
