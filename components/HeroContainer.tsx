91562463e8c3b921c9e0ef3f08aea7353df07e47
andrewrmn <andrewrmn@github.com>
2023-10-11 11:36:21 -0500

    Fix Primer build error

--- components/HeroContainer.tsx
+++ components/HeroContainer.tsx
@@ -8,7 +8,7 @@ export const HeroContainer = () => {
         <Hero.Description size="300">
           For Good First Issue empowers developers to collaborate on open source projects with missions that are driven by social impact. It is a curated list of open source projects focused on the <a href="https://digitalpublicgoods.net/digital-public-goods/" target="_blank" rel="noopener noreferrer">Digital Public Goods (DPGs)</a> that need your help! Together, we can drive positive and lasting contributions to the world, one commit at a time.
         </Hero.Description>
-        <Hero.PrimaryAction href="https://github.com/rubyforgood/happycommits" rel="noopener noreferrer">Contribute to a project</Hero.PrimaryAction>
+        <Hero.PrimaryAction href="https://github.com/rubyforgood/happycommits">Contribute to a project</Hero.PrimaryAction>
       </Hero>
     </>
   );
