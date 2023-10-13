82d0f2781d16316ce53ad7cd2a21dc79be07fc6c
Sean Marcia <smarcia@gmail.com>
2023-10-12 21:58:49 -0400

    image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,7 @@ export default function Document() {
           property="og:title"
           content="Happy Commits | Make your next open-source contribution matter."
         />
-        <meta property="og:image" content="https://happycommits.dev/meta.png" />
+        <meta property="og:image" content="https://happycommits.dev/" />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="happycommits.dev" />
@@ -28,7 +28,7 @@ export default function Document() {
           name="twitter:description"
           content="Making your next open-source contribution make the world better. Happy Commits is a curated list of accessible issues from open-source projects helping our communities. Start today!"
         />
-        <meta name="twitter:image" content="https://happycommits.dev/meta.png" />
+        <meta name="twitter:image" content="https://happycommits.dev/" />
 
         <meta charSet="UTF-8" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
