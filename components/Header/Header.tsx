ad004d2fbfd7e3f71bad32992fc3458aeb2f344c
Sean Marcia <seanmarcia@github.com>
2025-04-01 23:03:07 -0400

    Revert "Dark Mode issue#157"

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,13 +1,12 @@
 import styles from "./Header.module.scss";
-import Link from "next/link";
-import Image from "next/image";
-import { Button } from "@primer/react-brand";
-import { ThemeToggleButton } from "./ThemeToggleButton";
+import Link from 'next/link';
+import Image from 'next/image';
+import {Button} from '@primer/react-brand';
 
 export const Header = () => {
   return (
     <>
-      <header className={styles.siteHeader}>
+      <header className={styles.siteHeader} data-color-mode="dark">
         <div>
           <Link href="/" className={styles.homeLink}>
             <Image
@@ -17,18 +16,15 @@ export const Header = () => {
               alt="For Good First Issue logo"
             />
           </Link>
-          <div>
-            <Button
-              variant="primary"
-              as="a"
-              href="https://github.com/rubyforgood/happycommits/issues/new?assignees=&labels=💪+New+Project&projects=&template=suggest_project.yml&title=%5BNew+Project%5D%3A+%3Ctitle%3E"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className={styles.btnText}>Recommend a project</span>
-            </Button>
-            <ThemeToggleButton />
-          </div>
+          <Button
+            variant="primary"
+            as="a"
+            href="https://github.com/rubyforgood/happycommits/issues/new?assignees=&labels=💪+New+Project&projects=&template=suggest_project.yml&title=%5BNew+Project%5D%3A+%3Ctitle%3E"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className={styles.btnText}>Recommend a project</span>
+          </Button>
         </div>
       </header>
     </>
--- components/LanguageFilter.tsx
+++ components/LanguageFilter.tsx
@@ -1,5 +1,4 @@
 import Select from "react-select";
-import { ColorStyles } from "./ColorStyles";
 
 type LanguageFilterProps = {
   setSelectedLanguages: (languages: string[]) => void;
@@ -11,18 +10,9 @@ export const LanguageFilter = ({ setSelectedLanguages, languageOptions }: Langua
     <>
       <div>
         <label className="label">Language</label>
-        <Select
-          styles={ColorStyles}
-          isMulti
-          closeMenuOnSelect={false}
-          className=""
-          onChange={(selectedOptions) =>
-            setSelectedLanguages(selectedOptions.map((option) => option.value))
-          }
-          options={languageOptions}
-          classNamePrefix="select"
-        />
+        <Select isMulti closeMenuOnSelect={false} className="" onChange={(selectedOptions) => setSelectedLanguages(selectedOptions.map((option) => option.value))} options={languageOptions} classNamePrefix="select" />
       </div>
     </>
+
   );
 };
