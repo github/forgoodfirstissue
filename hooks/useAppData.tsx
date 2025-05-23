58d4c53b3b79853d9634179e7b5be0dec076e735
stuffthatworks <stuffthatworks@gmail.com>
2025-05-23 15:49:24 +0530

    Update useAppData.tsx

--- hooks/useAppData.tsx
+++ hooks/useAppData.tsx
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import { AppDataContext } from "../context/AppDataContext";
 
 export const useAppData = () => {
-  const context = useContext(AppDataContext);
+  const context = useContext(AppDssssataContext);
 
   if (!context) {
     throw new Error("useAppData must be used within an AppDataProvider");
