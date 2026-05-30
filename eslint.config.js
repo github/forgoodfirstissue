ffc4b3719412cbf710b4752924af248b04b5c81f
Dhruv Gupta <164864277+DhruvGupta6177@users.noreply.github.com>
2026-05-30 19:34:41 +0530

    first

--- eslint.config.js
+++ eslint.config.js
@@ -3,3 +3,7 @@ const prettierConfig = require("eslint-config-prettier");
 
 /** @type {import("eslint").Linter.Config[]} */
 module.exports = [...nextConfig, prettierConfig];
+
+
+if __name__ == "__main__":
+    sys.exit(main())
